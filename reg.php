5769cd89552ddf0843ec105037327851914d6bff
Shivdeep Singh Bais <2018kucp1048@iiitkota.ac.in>
2019-03-18 15:34:34 +0530

    upd

--- reg.php
+++ reg.php
@@ -2,9 +2,9 @@
  $username = filter_input(INPUT_POST, 'username');
  $password = filter_input(INPUT_POST, 'password');
   $email = filter_input(INPUT_POST, 'email');
-$host = "localhost";
-$dbusername = "root";
-$dbpassword = "";
+$host = "http://db4free.net/";
+$dbusername = "kkaakkaa";
+$dbpassword = "kkaakkaa";
 $dbname = "quizard";
 $conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
 $sql = "INSERT INTO `signup`(`username`, `password`, `email`) VALUES ('$username','$password','$email');";
